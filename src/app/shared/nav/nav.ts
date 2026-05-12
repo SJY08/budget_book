4733f46b9b543fc98500dc5fb70103dd2b0b5b05
서지유 <jihoseo2008@gmail.com>
2026-05-12 11:42:32 +0900

    feat :: nav component

--- src/app/shared/nav/nav.ts
+++ src/app/shared/nav/nav.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './nav.html',
-  styleUrl: './nav.css',
 })
-export class Nav {}
+export class NavComponent {
+  router = inject(Router);
+
+  isActive(path: string) {
+    return this.router.url.includes(path);
+  }
+
+  navItems = [
+    { path: '/home', icon: '🏠', label: '홈' },
+    { path: '/calendar', icon: '📆', label: '캘린더' },
+    { path: '/stats', icon: '📊', label: '통계' },
+  ];
+}
