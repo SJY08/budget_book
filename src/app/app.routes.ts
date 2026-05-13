a7f5198dba299be4a8e56cbd2afbe278632f7fbc
서지유 <jihoseo2008@gmail.com>
2026-05-13 14:16:12 +0900

    feat :: setting router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+// import { DashboardComponent } from './pages/dashboard/dashboard';
+// import { AddTransactionComponent } from './pages/add-transaction/add-transaction';
+// import { TransactionsComponent } from './pages/transactions/transactions';
+// import { AnalyticsComponent } from './pages/analytics/analytics';
+// import { CategoriesComponent } from './pages/categories/categories';
+// import { CategoryDetailComponent } from './pages/category-detail/category-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  //   { path: 'home', component: DashboardComponent },
+  //   { path: 'add', component: AddTransactionComponent },
+  //   { path: 'transactions', component: TransactionsComponent },
+  //   { path: 'analytics', component: AnalyticsComponent },
+  //   { path: 'categories', component: CategoriesComponent },
+  //   { path: 'category/:id', component: CategoryDetailComponent },
+  //   { path: '**', redirectTo: 'home' },
+];
