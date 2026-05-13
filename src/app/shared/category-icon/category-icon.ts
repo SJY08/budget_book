77d418c4d5b76dd2179ac00a6b7f6d4a61455d67
서지유 <jihoseo2008@gmail.com>
2026-05-13 13:44:29 +0900

    feat :: shared components codes

---
+++ src/app/shared/category-icon/category-icon.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-icon',
+  imports: [],
+  templateUrl: './category-icon.html',
+})
+export class CategoryIcon {}
---
+++ src/app/shared/transaction-item/transaction-item.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-item',
+  imports: [],
+  templateUrl: './transaction-item.html',
+})
+export class TransactionItem {}
