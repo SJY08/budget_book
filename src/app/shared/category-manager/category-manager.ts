290fb717ffb8b11e8c603493d169cdaaa03f2522
서지유 <jihoseo2008@gmail.com>
2026-05-13 09:18:12 +0900

    feat :: category component

--- src/app/shared/category-manager/category-manager.ts
+++ src/app/shared/category-manager/category-manager.ts
@@ -1,9 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { BudgetService } from '../../core/budget';
 
 @Component({
   selector: 'app-category-manager',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, RouterLink],
   templateUrl: './category-manager.html',
-  styleUrl: './category-manager.css',
 })
-export class CategoryManager {}
+export class CategoryManagerComponent {
+  budgetService = inject(BudgetService);
+
+  isAdding = signal(false);
+  newName = signal('');
+  newIcon = signal('💠');
+  newColor = signal('#6366f1');
+
+  iconOptions = [
+    '🍽️',
+    '🚌',
+    '🛍️',
+    '💰',
+    '🏥',
+    '🎮',
+    '☕',
+    '✈️',
+    '🏠',
+    '📚',
+    '💄',
+    '🎵',
+    '⚽',
+    '🐶',
+    '💡',
+  ];
+
+  add() {
+    if (!this.newName().trim()) return;
+    this.budgetService.addCategory(this.newName().trim(), this.newIcon(), this.newColor());
+
+    this.newName.set('');
+    this.newIcon.set('💠');
+    this.newColor.set('#6366f1');
+    this.isAdding.set(false);
+  }
+}
