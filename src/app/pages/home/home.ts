91f8e002715d44362db9cdfa4ec2abbd7264e373
서지유 <jihoseo2008@gmail.com>
2026-05-13 09:35:23 +0900

    feat :: home component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { BudgetFormComponent } from '../../shared/budget-form/budget-form';
+import { CategoryManagerComponent } from '../../shared/category-manager/category-manager';
+import { RouterLink } from '@angular/router';
+import { BudgetService } from '../../core/budget';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [BudgetFormComponent, CategoryManagerComponent, RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.css',
 })
-export class Home {}
+export class HomeComponent {
+  budgetService = inject(BudgetService);
+  showForm = signal(false);
+
+  thisMonth = new Date().toISOString().slice(0, 7);
+
+  thisMonthTransactions = () =>
+    this.budgetService.transactions().filter((t) => t.date.startsWith(this.thisMonth));
+
+  thisMonthIncom = () =>
+    this.thisMonthTransactions()
+      .filter((t) => t.type === 'income')
+      .reduce((s, t) => s + t.amount, 0);
+
+  thisMonthExpenese = () =>
+    this.thisMonthTransactions()
+      .filter((t) => t.type === 'expense')
+      .reduce((s, t) => s + t.amount, 0);
+
+  getCategoryById(id: string) {
+    return this.budgetService.categories().find((c) => c.id === id);
+  }
+
+  formatAmout(amount: number) {
+    return amount.toLocaleString('ko-KR') + '원';
+  }
+}
