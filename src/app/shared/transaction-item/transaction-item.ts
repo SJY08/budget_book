bbe7bb4789a68dfe958b3394c954c6bbfbcf582e
서지유 <jihoseo2008@gmail.com>
2026-05-13 14:24:56 +0900

    feat :: transaction item component

--- src/app/shared/transaction-item/transaction-item.ts
+++ src/app/shared/transaction-item/transaction-item.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { CategoryIconComponent } from '../category-icon/category-icon';
+import { KrwPipe } from '../pipes/krw-pipe';
+import { Category, Transaction } from '../../core/types';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [],
+  standalone: true,
+  imports: [CategoryIconComponent, KrwPipe],
   templateUrl: './transaction-item.html',
 })
-export class TransactionItem {}
+export class TransactionItemComponent {
+  transaction = input.required<Transaction>();
+  category = input.required<Category>();
+  showDelete = input<boolean>(false);
+  deleted = output<string>();
+}
