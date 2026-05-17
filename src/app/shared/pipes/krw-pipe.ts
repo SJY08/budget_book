b26f769e3a0761943bb63356897a8d0b98c22e84
서지유 <jihoseo2008@gmail.com>
2026-05-17 21:52:52 +0900

    feat :: not use \

--- src/app/shared/pipes/krw-pipe.ts
+++ src/app/shared/pipes/krw-pipe.ts
@@ -1,15 +1,8 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'krw',
-  standalone: true,
-})
+@Pipe({ name: 'krw', standalone: true })
 export class KrwPipe implements PipeTransform {
   transform(value: number): string {
-    return new Intl.NumberFormat('ko-KR', {
-      style: 'currency',
-      currency: 'KRW',
-      maximumFractionDigits: 0,
-    }).format(value);
+    return value.toLocaleString('ko-KR') + '원';
   }
 }
