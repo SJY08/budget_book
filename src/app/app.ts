90575d112c06e4abcd2bdd06b44dd2bc1319796e
서지유 <jihoseo2008@gmail.com>
2026-05-17 21:34:37 +0900

    feat :: pwa update setting

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { BottomNavComponent } from './shared/bottom-nav/bottom-nav';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +10,30 @@ import { BottomNavComponent } from './shared/bottom-nav/bottom-nav';
   imports: [RouterOutlet, BottomNavComponent],
   templateUrl: './app.html',
 })
-export class App {
+export class App implements OnInit {
   router = inject(Router);
+  swUpdate = inject(SwUpdate, { optional: true });
 
   isAddPage() {
     return this.router.url === '/add';
+  }
+
+  ngOnInit() {
+    if (!this.swUpdate?.isEnabled) return;
+
+    this.swUpdate.versionUpdates
+      .pipe(filter((e): e is VersionReadyEvent => e.type === 'VERSION_READY'))
+      .subscribe(() => {
+        this.swUpdate?.activateUpdate().then(() => {
+          document.location.reload();
+        });
+      });
+
+    setInterval(
+      () => {
+        this.swUpdate?.checkForUpdate();
+      },
+      30 * 60 * 1000,
+    );
   }
 }
